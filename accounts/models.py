68e8aaec3c268131a98d4f3007430edeb9d935ab
powerostad <90046706+Powerostad@users.noreply.github.com>
2023-10-22 16:22:56 +0330

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
---
+++ accounts/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from django.contrib.gis.geoip2 import GeoIP2
+
+# Create your views here.
